2ab797dd1439b830df791e07d54c8b76efc042f6
Rafael Ribeiro <rafaelsyprianoribeiro@hotmail.com>
2022-03-13 20:06:23 -0300

    Ajustes em commandos de reconhecimento de sons

--- wavdetect/src/convert-to-mono.py
+++ wavdetect/src/convert-to-mono.py
@@ -4,6 +4,7 @@
 
 # Import AudioSegment from pydub
 from pydub import AudioSegment
+from pydub.utils import make_chunks
 import os
 
 base_dir = '../../jupyter/work/dataset/learning/'
@@ -11,9 +12,26 @@ base_dir = '../../jupyter/work/dataset/learning/'
 paths = os.listdir(base_dir)
 
 
-def match_target_amplitude(sound, target_dBFS):
-    change_in_dBFS = target_dBFS - sound.max_dBFS
-    return sound.apply_gain(change_in_dBFS)
+def match_target_amplitude(audio, target_dBFS):
+    change_in_dBFS = target_dBFS - audio.max_dBFS
+    return audio.apply_gain(change_in_dBFS)
+
+
+def remove_initial_silence(audio, threshold=-12):
+    threshold_limit = False
+    temporary_raw_sample = []
+    chunk_length_ms = 10  # pydub calculates in millisecond
+    chunks = make_chunks(audio, chunk_length_ms)  # Make chunks of one sec
+    for i, chunk in enumerate(chunks):
+        raw_data = chunk.raw_data
+        if not threshold_limit:
+            chunk = AudioSegment(data=b''.join([raw_data]), sample_width=2, frame_rate=16000, channels=1)
+            if chunk.max_dBFS > threshold:
+                threshold_limit = True
+        else:
+            temporary_raw_sample.append(raw_data)
+
+    return AudioSegment(data=b''.join(temporary_raw_sample), sample_width=2, frame_rate=16000, channels=1)
 
 
 for path in paths:
@@ -32,6 +50,7 @@ for path in paths:
         split_audio = AudioSegment.from_wav(audio_file)
         split_audio = split_audio[0:200]
         split_audio_file = export_mono_split_path + "/" + file
+        # split_audio = remove_initial_silence(split_audio)
         split_audio.export(split_audio_file, format="wav")
 
         stereo_audio = AudioSegment.from_file(split_audio_file, format="wav")
@@ -43,3 +62,14 @@ for path in paths:
         export_normalized_file = os.path.join(export_normalized_path, file).replace(" ", "")
         normalized_sound = match_target_amplitude(sound, 0.0)
         normalized_sound.export(export_normalized_file, format="wav")
+
+    # for file_name in files:
+    #     file_path = os.path.join(base_dir + path, file_name)
+    #     audio_segment = AudioSegment.from_file(file_path, format="wav")
+    #     audio_segment = audio_segment.set_frame_rate(16000)
+    #     audio_segment = audio_segment.set_sample_width(2)
+    #     audio_segment = audio_segment.split_to_mono()
+    #     audio_segment = remove_initial_silence(audio_segment[0])
+    #     audio = audio_segment[:300]
+    #     export_mono_file = os.path.join(export_mono_path, file_name).replace(" ", "")
+    #     audio.export(export_mono_file, format='wav')
--- wavdetect/src/run.py
+++ wavdetect/src/run.py
@@ -3,40 +3,69 @@ import recognition
 from pydub import AudioSegment
 from pydub.utils import make_chunks
 
-file_name = './test/clipe3.wav'
+file_name = './test/clipe2.wav'
+
+source_snare = AudioSegment.from_wav("./source/snare.wav")
+source_kick = AudioSegment.from_wav("./source/kick.wav")
 
 audio = AudioSegment.from_file(file_name, "wav")
+audio = audio.set_frame_rate(16000)
+audio = audio.set_sample_width(2)
+audio = audio.split_to_mono()
+audio = audio[0]
 chunk_length_ms = 10  # pydub calculates in millisecond
 chunks = make_chunks(audio, chunk_length_ms)  # Make chunks of one sec
 
-threshold = False
+threshold_trigger = False
 temporary_raw_sample = []
 current_time = 0
 current_temporary_raw_sample_time = 0
 # Convert chunks to raw audio data which you can then feed to HTTP stream
+output_snare = AudioSegment(data=b''.join([]), sample_width=2, frame_rate=32000, channels=2)
+output_snare_positions = []
+
+output_kick = AudioSegment(data=b''.join([]), sample_width=2, frame_rate=32000, channels=2)
+output_kick_positions = []
+
 for i, chunk in enumerate(chunks):
     current_time += chunk_length_ms
     raw_audio_data = chunk.raw_data
     sound = AudioSegment(data=b''.join([raw_audio_data]), sample_width=2, frame_rate=16000, channels=1)
     if sound.max_dBFS > -6:
-        threshold = True
+        threshold_trigger = True
 
-    if threshold:
+    if threshold_trigger:
         if not temporary_raw_sample:
-            current_temporary_raw_sample_time = str(current_time / 1000)
+            current_temporary_raw_sample_time = current_time
         temporary_raw_sample.append(raw_audio_data)
         if len(temporary_raw_sample) >= 30:
-            temporary_sound = AudioSegment(data=b''.join(temporary_raw_sample), sample_width=2, frame_rate=16000, channels=1)
+            temporary_sound = AudioSegment(data=b''.join(temporary_raw_sample), sample_width=2, frame_rate=16000,
+                                           channels=1)
             temp_filename = './temp.wav'
             temporary_sound.export(temp_filename, format='wav')
             prediction = recognition.predict(temp_filename)
             if prediction == 'snare':
-                print(prediction, current_temporary_raw_sample_time)
-                os.rename(temp_filename, './output/snare'+str(current_temporary_raw_sample_time)+'.wav')
-                frames_after_predict = 0
+                # print(prediction, current_temporary_raw_sample_time / 1000)
+                # output_snare += AudioSegment.silent(chunk_length_ms)
+                output_snare_positions.append(current_temporary_raw_sample_time)
             if prediction == 'kick':
-                print(prediction, current_temporary_raw_sample_time)
-                os.rename(temp_filename, './output/kick' + str(current_temporary_raw_sample_time) + '.wav')
-                frames_after_predict = 0
+                # print(prediction, current_temporary_raw_sample_time / 1000)
+                # output_kick += source_kick
+                output_kick_positions.append(current_temporary_raw_sample_time)
+                # output_kick += AudioSegment.silent(chunk_length_ms)
             temporary_raw_sample = []
-            threshold = False
+            threshold_trigger = False
+    output_kick += AudioSegment.silent(chunk_length_ms)
+    output_snare += AudioSegment.silent(chunk_length_ms)
+
+for position in output_kick_positions:
+    output_kick = output_kick.overlay(source_kick, position=position)
+
+for position in output_snare_positions:
+    output_snare = output_snare.overlay(source_snare, position=position)
+
+
+output_snare.export('./output/snare.wav', format='wav')
+output_kick.export('./output/kick.wav', format='wav')
+print(output_snare_positions)
+print(output_kick_positions)
